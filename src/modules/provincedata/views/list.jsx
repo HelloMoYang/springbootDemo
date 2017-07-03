85baf7eb9abe155a9a315825525e354868ea8fa4
songxiaodong <826268557@qq.com>
2017-07-03 16:46:46 +0800

    。。。

--- src/modules/provincedata/views/list.jsx
+++ src/modules/provincedata/views/list.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-//import SectionHeader from '../../../components/section-header/index'
+import SectionHeader from '../../../components/section-header/index'
 import ProvinceTable from '../../../components/province-table/index'
 import ProvinceForm from'../../../components/province-form/index'
 
@@ -9,7 +9,8 @@ import '../style/index'
 class List extends React.Component {
     render() {
         return (
-            <div className="page">             
+            <div className="page"> 
+                <SectionHeader title="省数据配置"> </SectionHeader>            
                 <ProvinceTable/>
                 <ProvinceForm/>
             </div>

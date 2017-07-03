3a2de60e0fc4a48d3a06e9e80e75c75eed516c06
李继红 <707230595@qq.com>
2017-07-03 16:48:35 +0800

    页面背景fix

--- src/modules/provincedata/views/list.jsx
+++ src/modules/provincedata/views/list.jsx
@@ -1,17 +1,24 @@
 import * as React from 'react';
-//import SectionHeader from '../../../components/section-header/index'
+import SectionHeader from '../../../components/section-header/index'
 import ProvinceTable from '../../../components/province-table/index'
-import ProvinceForm from'../../../components/province-form/index'
-
-
-import '../style/index'
+import ProvinceForm from '../../../components/province-form/index'
 
 class List extends React.Component {
     render() {
         return (
-            <div className="page">             
-                <ProvinceTable/>
-                <ProvinceForm/>
+            <div className="page">
+                <div className="page-content">
+                    <div className='module-wapper'>
+                        <div className='module-wapper-inner'>
+                            <SectionHeader title='省数据'></SectionHeader>
+                            <section style={{ minHeight: `${window.innerHeight - 185}` }}>
+                                <ProvinceTable />
+                                <ProvinceForm />
+                            </section>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         );
     }

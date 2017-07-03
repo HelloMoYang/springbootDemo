8b2eb8fe289b137eeb977edd8388af18ab1fd705
李继红 <707230595@qq.com>
2017-07-03 17:56:36 +0800

    profilequery样式调整

--- src/modules/profilequery/views/list.jsx
+++ src/modules/profilequery/views/list.jsx
@@ -6,8 +6,6 @@ const TabPane = Tabs.TabPane;
 import ProfIccid from '../../../components/prof-iccid/index'
 import ProfEid from '../../../components/prof-eid/index'
 import ProfCount from '../../../components/prof-count/index'
-import '../style/index'
-
 
 class List extends React.Component {
     constructor(props) {
@@ -32,19 +30,25 @@ class List extends React.Component {
     render() {
         return (
             <div className="page profilequery">
-                <Tabs onChange={this.changeTab.bind(this)} >
-                    <TabPane tab=" 查询 ICCID " key="1">
-                        <ProfIccid searchData={this.searchData} />
+                <div className="page-content">
+                    <div className='module-wapper'>
+                        <div className='module-wapper-inner' style={{ minHeight: `${window.innerHeight - 88}` }}>
+                            <Tabs onChange={this.changeTab.bind(this)} >
+                                <TabPane tab=" 查询 ICCID " key="1">
+                                    <ProfIccid searchData={this.searchData} />
 
-                    </TabPane>
-                    <TabPane tab=" 查询 EID " key="2">
-                        <ProfEid searchData={this.searchData} />
+                                </TabPane>
+                                <TabPane tab=" 查询 EID " key="2">
+                                    <ProfEid searchData={this.searchData} />
 
-                    </TabPane>
-                    <TabPane tab=" Profile 统计 " key="3">
-                        <ProfCount />
-                    </TabPane>
-                </Tabs>
+                                </TabPane>
+                                <TabPane tab=" Profile 统计 " key="3">
+                                    <ProfCount />
+                                </TabPane>
+                            </Tabs>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }

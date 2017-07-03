c3572edc26c9298ea4853f4c89e004fbe23f6558
王曼玉 <manyu.wang@hpe.com>
2017-07-03 15:17:53 +0800

    ProfileQuery

--- src/modules/profilequery/views/list.jsx
+++ src/modules/profilequery/views/list.jsx
@@ -1,10 +1,47 @@
 import * as React from 'react';
+import * as Tabs from 'antd/lib/tabs'
+import 'antd/lib/tabs/style'
+const TabPane = Tabs.TabPane;
+
+import ProfIccid from '../../../components/prof-iccid/index'
+import ProfEid from '../../../components/prof-eid/index'
+import '../style/index'
+
 
 class List extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+    searchData(value, param) {
+        console.log(value, param);
+        // if (param == 'iccid') {
+        //     console.log(value, '---iccid');
+        // }
+
+    }
+
+    changeTab(key) {
+        console.log(key);
+    }
+
     render() {
         return (
-            <div className="page">
-                profilequery list
+            <div className="page profilequery">
+                <Tabs onChange={this.changeTab.bind(this)} >
+                    <TabPane tab=" 查询 ICCID " key="1">
+                        <ProfIccid searchData={this.searchData} />
+
+                    </TabPane>
+                    <TabPane tab=" 查询 EID " key="2">
+                        <ProfEid searchData={this.searchData} />
+
+                    </TabPane>
+                    <TabPane tab=" Profile 统计 " key="3">Content of Tab Pane 3</TabPane>
+                </Tabs>
             </div>
         );
     }

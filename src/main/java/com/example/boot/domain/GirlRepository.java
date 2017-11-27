f45b6f568d42853980a6479f663b33eecf9174ef
杨沫 <374577605@qq.com>
2017-11-27 22:42:58 +0800

    Revert "Revert "数据库操作3""
    This reverts commit ddc6319df88c56d85290ca35a6155fa574176d5a.

--- src/main/java/com/example/boot/domain/GirlRepository.java
+++ src/main/java/com/example/boot/domain/GirlRepository.java
@@ -1,7 +1,11 @@
 package com.example.boot.domain;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 													//类名，Id类型
 public interface GirlRepository extends JpaRepository<Girl, Integer>{
 	
+	//	通过年龄查询
+	public List<Girl> findByAge(Integer age);
 }
